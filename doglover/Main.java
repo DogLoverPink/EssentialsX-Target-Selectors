244a20cacfab69891b43ba0c015c1b8aa95aa539
DogLoverPink <64995932+DogLoverPink@users.noreply.github.com>
2022-05-27 19:13:58 -0400

    Update Main for v1.6

--- doglover/Main.java
+++ doglover/Main.java
@@ -3,13 +3,16 @@ package doglover;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import doglover.Events.CommandExecute;
+import doglover.Events.PlayerCommandExecute;
+import doglover.Events.ServerCommandExecute;
 import doglover.Events.TabEvent;
 
 public class Main extends JavaPlugin{
+    //private static ConversationFactory factory = new ConversationFactory(doglover.Events.ServerCommandExecute.plugin);
 		@Override
 		public void onEnable() {
-            getServer().getPluginManager().registerEvents(new CommandExecute(this), this);
+            getServer().getPluginManager().registerEvents(new ServerCommandExecute(), this);
+            getServer().getPluginManager().registerEvents(new PlayerCommandExecute(this), this);
             getServer().getPluginManager().registerEvents(new TabEvent(), this);
             if (getServer().getPluginManager().getPlugin("Essentials") == null) {
                 Bukkit.getLogger().warning("-------------------");
@@ -25,8 +28,8 @@ public class Main extends JavaPlugin{
                 Conversation conversation = factory.buildConversation(all);
                 factory.withFirstPrompt(doglover.ConverseThing.noChat).withEscapeSequence("cancel");
         		conversation.begin();
-                Bukkit.getScheduler ().runTaskLater (doglover.Events.CommandExecute.plugin, () -> all.performCommand("give @a NeededForSomeStrangeReason,IDKWhy"), 02L);
-                Bukkit.getScheduler ().runTaskLater (doglover.Events.CommandExecute.plugin, () -> all.abandonConversation(conversation), 02L);
+                Bukkit.getScheduler ().runTaskLater (doglover.Events.ServerCommandExecute.plugin, () -> all.performCommand("give @a NeededForSomeStrangeReason,IDKWhy"), 02L);
+                Bukkit.getScheduler ().runTaskLater (doglover.Events.ServerCommandExecute.plugin, () -> all.abandonConversation(conversation), 02L);
                 
             }*/
     }
